dce6d6d814dcd8e7db3429f2abcc32a6244faa07
DeleMike <akindelemichael65@gmail.com>
2022-07-05 23:00:55 +0100

    add product data

---
+++ lib/data/product_data.dart
@@ -0,0 +1,20 @@
+import '../models/product.dart';
+
+final products = [
+  Product(productId: 1, name: 'Sweater', description: 'A sweater', price: 5000),
+  Product(productId: 2, name: 'Gown', description: 'A gown', price: 5000),
+  Product(productId: 3, name: 'Smart Watch', description: 'A smart watch', price: 10000),
+  Product(productId: 4, name: 'Glasses', description: 'A glasses', price: 2500),
+  Product(productId: 5, name: 'Shirt', description: 'A shirt', price: 5000),
+  Product(productId: 6, name: 'Headset', description: 'A headset', price: 8000),
+  Product(productId: 7, name: 'Bag', description: 'A bag', price: 3500),
+  Product(productId: 8, name: 'Football boots', description: 'A football boot', price: 4000),
+  Product(productId: 9, name: 'High heels', description: 'A high heel shoe', price: 7000),
+  Product(productId: 10, name: 'Mouse', description: 'A mouse', price: 2900),
+  Product(productId: 11, name: 'Ear Pods', description: 'An Ear pod', price: 6000),
+  Product(productId: 12, name: 'Smart Watch', description: 'A smart watch', price: 12000),
+  Product(productId: 13, name: 'Laptop', description: 'A laptop', price: 400000),
+  Product(productId: 14, name: 'Smart Watch', description: 'A smart watch', price: 5000),
+  Product(productId: 15, name: 'XBox Pad', description: 'An XBox Pad', price: 15000),
+  Product(productId: 16, name: 'Phone', description: 'A phone', price: 95000),
+];
---
+++ lib/models/product.dart
@@ -0,0 +1,16 @@
+/// Model for a single Product
+///
+class Product {
+  /// product's unique identifier.
+  final int productId;
+  /// product's name
+  final String name;
+  /// product's description
+  final String description;
+  /// product's price
+  final double price;
+
+  /// Model for a single Product
+  ///
+  Product({required this.productId, required this.name, required this.description, required this.price});
+}
